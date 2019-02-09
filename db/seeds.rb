10fa5381233a605331c6a5611508c3c78a2ddf8e
vilson junior <vilsonrjunior@gmail.com>
2019-02-09 17:48:54 +0800

    added link to reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+restaurants_attributes = [
+  { name:         'Dishroom',
+    address:      '7 St, London E2 7JE',
+    phone_number:   01234566,
+    category:      'italian'
+  },
+
+  {
+    name:         'Pizza place',
+    address:      '8 Boundary St, London E2 7JE',
+    phone_number:   012343356,
+    category:      'italian'
+  },
+
+  {
+    name:         'French',
+    address:      '10 Boundary St, London E2 7JE',
+    phone_number:  0122323456,
+    category:      'french'
+  },
+
+  {
+    name:         'Japanese',
+    address:      '12 Boundary St, London E2 7JE',
+    phone_number:   012543456,
+    category:      'japanese'
+  },
+
+  {
+    name:         'Pasta place',
+    address:      '13 Boundary St, London E2 7JE',
+    phone_number:   0123455545,
+    category:      'italian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
