51193b0df3219ff90bef5ae808d58baeab58e826
vilson junior <vilsonrjunior@gmail.com>
2019-02-10 14:34:17 +0800

    changed paths

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,6 @@
 class Restaurant < ApplicationRecord
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+  validates :name, :address, presence: :true
+  validates :category, inclusion: { in: CATEGORIES }
   has_many :reviews, dependent: :destroy
-
-  validates :name, uniqueness: true, presence: true
-  validates :address, presence: true, uniqueness: true
-  validates :phone_number, presence: true
-  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,7 @@
-Rails.application.routes.draw do
-  resources :restaurants do
-    resources :reviews, only: [ :index, :new, :create ]
-  end
-  resources :reviews, only: [ :show, :edit, :update, :destroy ]
 
- namespace :admin do
-    resources :restaurants, only: [:index]
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:create, :new ]
   end
 end
-
-
-# Rails.application.routes.draw do
-#   resources :restaurants do
-#     resources :reviews, only: [ :new, :create ]
-#   end
-# end
